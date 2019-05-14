32214b1aea41d76e2ea852b0b37fb037a66da475
tchang9 <tlchang@gmail.com>
2019-05-14 10:04:36 -0400

    created simple header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div style={{left: 160}} className="ui top fixed menu">
+            <p className="item">Japan 12/19</p>
+            <p className="item">December 12th, 2019</p>
+            <p className="item">Tokyo, Japan</p>
+            <button className="item">Edit Event</button>
+            <button className="item">Tony Chang</button>
+        </div>
+    )
+}
+
+export default Header

4d04ed8230f8c4e73cf8b9805113e2ff51a21cc1
tchang9 <tlchang@gmail.com>
2019-05-15 11:19:43 -0400

    set up simple react router

--- src/containers/BodyContainer.js
+++ src/containers/BodyContainer.js
@@ -1,13 +1,23 @@
 import React from 'react'
-// import TopicsContainer from './TopicsContainer'
-// import TopicContainer from './TopicContainer'
+import { Route, Switch } from 'react-router-dom'
+import TopicContainer from './TopicContainer'
 import ScheduleContainer from './ScheduleContainer'
+import TopicsContainer from './TopicsContainer'
+import AddTopicForm from '../components/AddTopicForm'
+import AddActivityForm from '../components/AddActivityForm'
+
 
 const BodyContainer = () => {
     return (
-        <div> 
-            <ScheduleContainer />
-        </div>
+        <> 
+            <Switch>
+                <Route path='/profile/event1/topic1/add' component = {AddTopicForm} />
+                <Route path='/profile/event1/topic1' component = {TopicContainer} />
+                <Route path='/profile/event1/schedule/add' component = {AddActivityForm} />
+                <Route path='/profile/event1/schedule' component = {ScheduleContainer} />
+                <Route path='/profile/event1' component = {TopicsContainer} />
+            </Switch>
+        </>
     )
 }
 

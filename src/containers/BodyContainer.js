fe0ae27b6311a1f811ba8d935f38d7f11661c3a8
tchang9 <tlchang9@gmail.com>
2019-09-13 16:00:59 -0400

    Removing unused code

--- src/containers/BodyContainer.js
+++ src/containers/BodyContainer.js
@@ -3,10 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import TopicContainer from './TopicContainer'
 import ScheduleContainer from './ScheduleContainer'
 import TopicsContainer from './TopicsContainer'
-import AddTopicForm from '../components/AddTopicForm'
-import AddActivityForm from '../components/AddActivityForm'
 import EditEventForm from '../components/EditEventForm'
-import AddEventForm from '../components/AddEventForm'
 import ParticipantsContainer from './ParticipantsContainer'
 
 
@@ -14,12 +11,8 @@ const BodyContainer = () => {
     return (
         <> 
             <Switch>
-                <Route path='/profile/events/add' component = {AddEventForm} />    
                 <Route path='/profile/events/edit/:event' component = {EditEventForm} />
-                <Route path='/profile/events/:event/topics/add' component = {AddTopicForm} />
                 <Route path='/profile/events/:event/topics/:topic' component = {TopicContainer} />
-                <Route path='/profile/events/:event/schedule/add' component = {AddActivityForm} />
-                {/* <Route path='/profile/events/:event/schedule/activities/edit/:activity' component = {AddActivityForm} /> */}
                 <Route path='/profile/events/:event/participants' component = {ParticipantsContainer} />
                 <Route path='/profile/events/:event/schedule' component = {ScheduleContainer} />
                 <Route path='/profile/events/:event/topics' component = {TopicsContainer} />
